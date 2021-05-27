72f90284eb3f7146b7c20500b35827286cdc631a
Lucas Leite <lucasleite91@gmail.com>
2021-05-27 15:05:29 +0100

    finished the migrations, models and seed with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database first"
+Movie.destroy_all
+
+puts "Creating Movies..."
+10.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    rating: Faker::Number.within(range: 0.0..10.0),
+    poster_url: "https://picsum.photos/seed/#{Faker::Movies::LordOfTheRings.character}/200/300"
+  )
+  movie.save!
+end
+
+puts "Movies created!"
